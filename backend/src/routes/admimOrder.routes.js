d9c0d6ad1dc3c887c1772bd53e2b809f844fa3ba
Abhishek280999 <darwekar155@gmail.com>
2024-02-15 14:32:00 +0530

    Route done

--- backend/src/routes/admimOrder.routes.js
+++ backend/src/routes/admimOrder.routes.js
@@ -10,3 +10,7 @@ router.put("/:orderId/shio", authenticate,orderController.shippOrder)
 router.put("/:orderId/deliver", authenticate,orderController.deliverOrder)
 router.put("/:orderId/cancel", authenticate,orderController.cancelledOrder)
 router.put("/:orderId/delete", authenticate,orderController.deleteOrder)
+
+
+
+module.exports= router;
