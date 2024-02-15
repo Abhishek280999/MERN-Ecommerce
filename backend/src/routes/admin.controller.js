38bda18f0e9a1a7340acd5d20202846966080995
Abhishek280999 <darwekar155@gmail.com>
2024-02-15 14:18:55 +0530

    h

---
+++ backend/src/routes/admin.controller.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const router=express.Router();
+const orderController = require("../controller/adminOrderController");
+const authenticate = require("../middleware/authenticat");
+
+
+router.get("/",authenticate,orderController.getAllOrders)
+router.put("/:orderId/confirmed", authenticate,orderController.confirmedOrder)
+router.put("/:orderId/shio", authenticate,orderController.shippOrder)
+router.put("/:orderId/deliver", authenticate,orderController.deliverOrder)
+router.put("/:orderId/cancel", authenticate,orderController.cancelledOrder)
+router.put("/:orderId/delete", authenticate,orderController.deleteOrder)
